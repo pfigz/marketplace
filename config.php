f8c66773ccc0fccc10d797b3a61f4dc889c5d068
pfigz <pfigdev@gmail.com>
2021-09-14 12:27:48 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 
-define('DB_HOST', 'localhost:3307');
-define('DB_NAME', 'marketplace');
-define('DB_USER', 'test');
-define('DB_PASS', 'password');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 define('SHOW_ERROR_DETAIL', true);
