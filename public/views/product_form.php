bfe17957f4e838fd860ae9dd06d5b7a1ed61bebc
pfigz <pfigdev@gmail.com>
2022-03-15 00:10:40 +0100

    Remove htmlspecialchars from product_form.php

--- public/views/product_form.php
+++ public/views/product_form.php
@@ -3,28 +3,28 @@
         <div class="form-group mb-3">
             <div>
                 <label for="productName" class="form-label">Product Namet</label>
-                <input type="text" class="form-control" name="productName" id="productName" placeholder="Product Name" value="<?= htmlspecialchars($product->productName); ?>">
+                <input type="text" class="form-control" name="productName" id="productName" placeholder="Product Name" value="<?= $product->productName; ?>">
             </div>
         </div>
 
         <div class="form-group mb-3">
             <div>
                 <label for="stock" class="form-label">Product Stock</label>
-                <input type="text" class="form-control" name="stock" id="stock" placeholder="Stock Amount" value="<?= htmlspecialchars($product->stock); ?>">
+                <input type="text" class="form-control" name="stock" id="stock" placeholder="Stock Amount" value="<?= $product->stock; ?>">
             </div>
         </div>
 
         <div class="form-group mb-3">
             <div>
                 <label for="price" class="form-label">Product Price</label>
-                <input type="text" class="form-control" name="price" id="price" placeholder="Price" value="<?= htmlspecialchars($product->price); ?>">
+                <input type="text" class="form-control" name="price" id="price" placeholder="Price" value="<?= $product->price; ?>">
             </div>
         </div>
 
         <div class="form-group mb-3">
             <div>
                 <label for="details" class="form-label">Product details</label>
-                <input type="text" class="form-control" name="details" id="details" placeholder="Type product details here" value="<?= htmlspecialchars($product->details); ?>">
+                <input type="text" class="form-control" name="details" id="details" placeholder="Type product details here" value="<?= $product->details; ?>">
             </div>
         </div>
 
