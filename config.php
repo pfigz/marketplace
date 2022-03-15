96f2f091aa76c9b4913025c31f108a7cb4636fda
pfigz <pfigdev@gmail.com>
2022-03-15 21:39:39 +0100

    Testing heroku config variables

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 
-getenv('CLEARDB_DATABASE_URL');
-getenv('DB_NAME');
-getenv('DB_USER');
-getenv('DB_PASS');
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
 
-getenv('SHOW_ERROR_DETAIL');
+define('SHOW_ERROR_DETAIL', getenv('SHOW_ERROR_DETAIL'));
