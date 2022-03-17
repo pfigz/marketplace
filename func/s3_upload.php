50781e89434d68c8e8e1e64c0d5a20bb61881b3e
pfigz <pfigdev@gmail.com>
2022-03-17 21:44:31 +0100

    Big fix, modify paths on require files

--- func/s3_upload.php
+++ func/s3_upload.php
@@ -1,7 +1,7 @@
 <?php
 
-require '../includes/init.php';
-require '../vendor/autoload.php';
+require '../../includes/init.php';
+require '../../vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
