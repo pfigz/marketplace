a0bc2307f2bf0fa76d41d13984c87d39544520a8
pfigz <pfigdev@gmail.com>
2022-03-18 01:11:40 +0100

    Bug fix 5, altered if statement

--- func/view_cart.php
+++ func/view_cart.php
@@ -7,18 +7,17 @@ $cart = new Cart;
 $auth = new Auth;
 
 if (! isset($_SESSION['quantity']) && ! isset($_SESSION['price'])) {
-    
+
     $_SESSION['quantity'] = [];
     $_SESSION['price'] = [];
+    
+} 
 
-} else {
+// Set product ids in Session to a variable 
+$cart_ids = isset($_SESSION['quantity']) ? $_SESSION['quantity'] : array();
 
-    // Set product ids in Session to a variable 
-    $cart_ids = isset($_SESSION['quantity']) ? $_SESSION['quantity'] : array();
+// Associative array of products in the cart
+$products = $cart->viewCart($conn, $_SESSION['quantity']);
 
-    // Associative array of products in the cart
-    $products = $cart->viewCart($conn, $_SESSION['quantity']);
-
-    // The subtotal of items in the cart
-    $subtotal = $cart->subtotal($_SESSION['quantity'], $_SESSION['price']);
-}
+// The subtotal of items in the cart
+$subtotal = $cart->subtotal($_SESSION['quantity'], $_SESSION['price']);
