bc21ff369272a2506ec01bd79d77d83cdf6588db
pfigz <pfigdev@gmail.com>
2022-03-21 00:46:21 +0100

    Add login link to nav

--- includes/header.php
+++ includes/header.php
@@ -29,6 +29,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/public/views/cart.php">Shopping Cart</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/public/views/login.php">Login</a>
+                        </li>
 
                         <?php else: ?>
 
