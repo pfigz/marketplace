5ecfe61ace36ff9cc8906ed229b7b2e26fe89bb5
pfigz <pfigdev@gmail.com>
2022-03-22 17:24:54 +0100

    Bug fix for Ajax request

--- func/update_quantity.php
+++ func/update_quantity.php
@@ -2,19 +2,7 @@
 
 require '../includes/init.php';
 
-$url = new Url;
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    $quantities = $_SESSION['quantity'];
-    $updatedQuantities = $_POST;
-
-    $_SESSION['quantity'] = array_replace($quantities, $updatedQuantities);
-
-    $url->redirect('/public/views/cart.php');
-
+if ($_SERVER['REQUEST_METHOD'] == "POST") 
+{
+    $_SESSION['quantity'] = array_replace($_SESSION['quantity'], $_POST);
 }
-
-
-
-
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,27 +1,35 @@
 
 function updateCart() {
 
-    // Get the quantities
+    // Get the product ids in an array
+    let productIds = [];
+    document.querySelectorAll(".productID").forEach(function (element) {
+        let id = element.value;
+            productIds.push(id);
+            return productIds;
+    });
+
+    // Get the product quantities from shopping cart in an array
     let quantities = [];
     document.querySelectorAll(".quantity").forEach(function (element) {
         let quantity = element.value;
             quantities.push(quantity);    
     });
 
-    // Get the prices
+    // Get the product prices in an array
     let prices = [];
     document.querySelectorAll(".price").forEach(function (item) {
         let price = item.innerHTML;
         prices.push(price);
     });
 
-    // Multiply the quantities * price arrays
+    // Multiply the quantities * prices arrays
     let totals = [];
     for (let i = 0; i < Math.min(quantities.length, prices.length); i++) {
         totals[i] = quantities[i] * prices[i];
     }
 
-    // Get sum of array items = Subtotal
+    // Get sum of totals array items
     let subtotal = 0;
     for (let i = 0; i < totals.length; i++) {
         subtotal += totals[i];
@@ -30,77 +38,31 @@ function updateCart() {
     // Display new Subtotal
     document.getElementById("subtotal").innerHTML = subtotal;
 
-    // console.log(quantities);
+    // Map a new object using productIds and updated quantities
+    function updatedQuantities(productIds, quantities) {
+        if (productIds.length != quantities.length || productIds.length == 0 || quantities.length == 0) {
+            return null;
+        }
+        let updatedArray = {};
+
+        productIds.forEach((k, i) => {updatedArray[k] = quantities[i]})
+        return updatedArray;
+    }
+
+    // Set new object as variable to be passed in Ajax request
+    let newQuantities = updatedQuantities(productIds, quantities);
 
     // JQuery Ajax Request
-    // $.ajax({
-    //     type: "POST",
-    //     url: "/../../func/update_quantity.php",
-    //     data: $(quantities),
-    //     success: function() {
-    //         console.log(quantities);
-    //     }
-    // });
+    $.ajax({
+        type: "POST",
+        url: "/../../func/update_quantity.php",
+        dataType: 'json',
+        data: newQuantities,
+        success: function() {
+        }
+    });
 }
 
-$("#productForm").validate({
-    rules: {
-        productName: {
-            required: true
-        },
-        stock: {
-            required: true
-        },
-        price: {
-            required: true
-        },
-        details: {
-            required: true
-        },
-        image: {
-            required: false
-        }
-    }
-});
 
-$("#signUp").validate({
-    rules: {
-        username: {
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true
-        }
-    }
-});
-
-$("#login").validate({
-    rules: {
-        username: {
-            required: true
-        },
-        password: {
-            required: true
-        }
-    }
-});
-
-$("#submitOrder").validate({
-    rules: {
-        address: {
-            required: true
-        },
-        street: {
-            required: true
-        },
-        zipcode: {
-            required: true
-        }
-    }
-});
 
 
