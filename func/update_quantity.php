5bdca82a5ee10aa608366530c3da17c54a0f0f44
pfigz <pfigdev@gmail.com>
2022-03-28 22:10:00 +0200

    Bug fix, update_quantity script is inconsistent

--- func/update_quantity.php
+++ func/update_quantity.php
@@ -2,7 +2,7 @@
 
 require '../includes/init.php';
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") 
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_SESSION['quantity'])) 
 {
     $_SESSION['quantity'] = array_replace($_SESSION['quantity'], $_POST);
 }
