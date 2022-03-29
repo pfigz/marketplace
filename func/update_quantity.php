324b3fd3ff23831f059af188ec8f07340bd7db1a
pfigz <pfigdev@gmail.com>
2022-03-29 02:15:45 +0200

    Bug fix attempt 2, update quantity in cart is not updating session var

--- func/update_quantity.php
+++ func/update_quantity.php
@@ -2,7 +2,7 @@
 
 require '../includes/init.php';
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_SESSION['quantity'])) 
+if ($_SERVER['REQUEST_METHOD'] == "POST") 
 {
     $_SESSION['quantity'] = array_replace($_SESSION['quantity'], $_POST);
 }
