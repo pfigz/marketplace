cb736df56f864a1872a706dc87fd403061c0d2db
pfigz <pfigdev@gmail.com>
2022-03-31 16:10:11 +0200

    Add icon and update layout of index.php

--- index.php
+++ index.php
@@ -18,16 +18,26 @@ $auth = new Auth;
         <h2>Welcome to The Market Place</h2>
     </div> 
 
-    <div class="contaienr d-flex justify-content-center">
-        <h3>Please <a href="public/views/sign_up.php">signup</a> or <a href="public/views/login.php">login</a> to add products</h3>
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="container d-flex justify-content-center mb-5">  
+            <img src="public/assets/images/site/online-shopping.png" alt="An online shopping icon">
+        </div> 
+            <h4>Please <a href="public/views/sign_up.php">signup</a> or <a href="/public/views/login.php">login</a> to add products and submit comments</h4>      
     </div>
     
 <?php endif; ?>
 
 <?php if ($auth->isLoggedIn()): ?>
 
-    <div class="container d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-center">
         <h2>Welcome to the Market Place <?= htmlspecialchars($_SESSION['username']); ?></h2>
+    </div>
+
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="container d-flex justify-content-center mb-5">  
+            <a href="public/views/products.php"><img src="/public/assets/images/site/online-shopping.png" alt="An online shopping icon"></a>
+        </div> 
+            <h5>Thanks for signing in! Click the icon to view products, or click Add Product above to add your own!</h5>      
     </div>
 
 <?php endif; ?>
