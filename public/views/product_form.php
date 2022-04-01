7b72a2155c49f5b1017d4bc82ecd08b07874faea
pfigz <pfigdev@gmail.com>
2022-04-02 01:27:39 +0200

    Changes to product form to specify if adding or editing a product

--- public/views/product_form.php
+++ public/views/product_form.php
@@ -1,45 +1,49 @@
-<div class="container d-flex justify-content-center">
-    <form autocomplete="off" method="POST" enctype="multipart/form-data" id="productForm">
-        <div class="form-group mb-3">
-            <div>
-                <label for="productName" class="form-label">Product Name</label>
-                <input type="text" class="form-control" name="productName" id="productName" placeholder="Product Name" value="<?= $product->productName; ?>">
-            </div>
+<div class="container d-flex justify-content-center pt-5">
+    <div class="card center-block" style="background-color: #eeeeee;">
+        <div class="d-flex justify-content-center mt-2">
+            <?php if ( $product->productID == NULL ) : ?> 
+                <h3>Add Product Form</h3>                    
+            <?php else : ?>
+                <h3>Edit Product Form</h3>
+            <?php endif; ?>
         </div>
-
-        <div class="form-group mb-3">
-            <div>
-                <label for="stock" class="form-label">Product Stock</label>
-                <input type="text" class="form-control" name="stock" id="stock" placeholder="Stock Amount" value="<?= $product->stock; ?>">
+        <form autocomplete="off" method="POST" enctype="multipart/form-data" id="productForm">
+            <div class="form-group mb-3">
+                <div>
+                    <label for="productName" class="form-label">Product Name</label>
+                    <input type="text" class="form-control" name="productName" id="productName" placeholder="Product Name" value="<?= $product->productName; ?>">
+                </div>
             </div>
-        </div>
-
-        <div class="form-group mb-3">
-            <label for="price" class="form-label">Product Price</label>
-            <div class="input-group mb-3">
-                <span class="input-group-text">$</span>
-                <input type="text" class="form-control" name="price" id="price" placeholder="Price" value="<?= $product->price; ?>">
+            <div class="form-group mb-3">
+                <div>
+                    <label for="stock" class="form-label">Product Stock</label>
+                    <input type="text" class="form-control" name="stock" id="stock" placeholder="Stock Amount" value="<?= $product->stock; ?>">
+                </div>
             </div>
-        </div>
-
-        <div class="form-group mb-3">
-            <label for="details" class="form-label">Product details</label>
-            <div class="input-group">
-                <textarea class="form-control" name="details" id="details" placeholder="Type product details here" aria-label="Product details"><?= $product->details; ?></textarea>
+            <div class="form-group mb-3">
+                <label for="price" class="form-label">Product Price</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">$</span>
+                    <input type="text" class="form-control" name="price" id="price" placeholder="Price" value="<?= $product->price; ?>">
+                </div>
             </div>
-        </div>
-
+            <div class="form-group mb-3">
+                <label for="details" class="form-label">Product details</label>
+                <div class="input-group">
+                    <textarea class="form-control" name="details" id="details" placeholder="Type product details here" aria-label="Product details"><?= $product->details; ?></textarea>
+                </div>
+            </div>
         
-        <div class="form-group mb-3">
-            <div>
-                <small>Image uploads currently disabled</small>
-                <!-- <label for="image" class="form-label">Product Image</label> -->
-                <input type="file" class="form-control" name="image" id="image" disabled>
+            <div class="form-group mb-3">
+                <div>
+                    <small>Image uploads currently disabled</small>
+                    <!-- <label for="image" class="form-label">Product Image</label> -->
+                    <input type="file" class="form-control" name="image" id="image" disabled>
+                </div>
             </div>
-        </div>
-
-        <div class="form-group d-flex justify-content-center">
-            <button class="btn btn-dark" type="submit">Submit</button>
-        </div>
-    </form>
+            <div class="form-group d-flex justify-content-center">
+                <button class="btn btn-dark" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
 </div>
